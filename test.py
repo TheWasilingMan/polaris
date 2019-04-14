6ff9710a1c408c946836cb993911b1ae8bf7b69f
TheWasilingMan <wasil@usc.edu>
2019-04-14 04:56:39 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -8,26 +8,22 @@ while outter_count >= 0:
 
 	count = 15
 	result = 0
-	divisor_sum = 0
 	mul_sum = 0
 	while count >= 0:
-		#if count == 1:
-		#	continue
-		#divisor = 1/(1+(count/(1-count))**(-3))
-		#result += divisor * array[count]
-		#divisor_sum += divisor
 		result += array[count]*mul[count]
 		mul_sum += mul[count]
 		count -= 1
-		
-	#result //= 15 + divisor_sum
 	result //= mul_sum
 	print(result)
 	outter_count -= 1
+	
 	#if outter_count >= 10:
-	array[outter_count+1] = 140
+		#array[outter_count+1] = 140
 	#else:
 	#	array[outter_count+1] = 140
 	#	array[outter_count+6] = 155
+	
+	array[outter_count+1] = 140
+	
 	#print(array)
 	
